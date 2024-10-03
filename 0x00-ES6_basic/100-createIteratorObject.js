3e1d42fd4087cb36555f5cf76b312974878a97be
Adeboga Abigail <abbyboga@gmail.com>
2024-10-03 13:15:11 -0700

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   let employees = [];
-  for (const value of Object.values(report.allEmployees) {
-    employees = [...employees, ...value];
+  for (const value of Object.values(report.allEmployees)) {
+    employees.push(value);
   }
 
   return employees;
