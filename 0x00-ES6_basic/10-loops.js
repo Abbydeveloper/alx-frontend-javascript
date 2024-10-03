60cc0237fc63cd17e0c5577a910832adf1c4904f
Adeboga Abigail <abbyboga@gmail.com>
2024-10-03 10:33:06 -0700

    For...of loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array) {
-    let value = array[idx];
+  for (const value of array) {
+    let idx = array.indexOf(value);
     array[idx] = appendString + value;
   }
 
