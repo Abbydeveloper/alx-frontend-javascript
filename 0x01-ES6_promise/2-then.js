314d33d7251738b6d36b03d11a494aa534dbd2a1
Adeboga Abigail <abbyboga@gmail.com>
2024-10-05 03:46:09 -0700

    Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  return Promise
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    }
+  )
+}
