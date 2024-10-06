5a7c56211764472fe31e374443a0b98deab3d852
Adeboga Abigail <abbyboga@gmail.com>
2024-10-05 19:22:55 -0700

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhote from './5-photo-reject.js';
+
+export default asyncfunction handleProfileSignup(firstName, lastName,, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName))
+    .then(response => (
+      response.map(data => ({
+        status: data.status,
+        value = data.status === 'fulfilled' ? data.value : String(data.reason)
+      }))
+    ));
+}
