6e903929838075d6ca833a84f9175f8b5f34eb9d
Adeboga Abigail <abbyboga@gmail.com>
2024-10-05 19:11:27 -0700

    Simple Promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,7 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve) => {
-    {firstName, lastName,},
-  };
+  return Promise.resolve(
+    {
+      firstName, lastName,
+    },
+  );
 }
