772adadbe6d78f081800bbee41d1a6569e1eeba1
Adeboga Abigail <abbyboga@gmail.com>
2024-10-08 13:26:13 -0700

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,9 +2,10 @@ export default function updateStudentGradeByCity(arr, city, newGrades) {
   const cityStudents = arr.filter((student) => student.location === city);
   return cityStudents.map((student) => {
     const newRecord = student;
+    newRecord.grade = 'N/A';
     for (const grade of newGrades) {
       if (student.id === grade.studentId) {
-        newRecord.grade = grade.grade ? grade.grade : 'N/A';
+        newRecord.grade = grade.grade;
       }
     }
     return newRecord;
