9e4a6bb7b79b7378739b3fb59da3e0c7aee20527
Adeboga Abigail <abbyboga@gmail.com>
2024-10-08 14:31:05 -0700

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, startString) {
-  const newString = []
-  if (!startString || typeof startString !== 'string')
+  const newString = [];
+  if (!startString || typeof startString !== 'string') {
     return '';
+  }
   for (const setVal of set.values()) {
     if (setVal.startsWith(startString, 0)) {
       newString.push(setVal.slice(startString.length));
