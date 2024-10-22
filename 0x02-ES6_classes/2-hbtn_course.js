0cdf0e521099820d2da4196cdce5d9ad48a46f85
Adeboga Abigail <abbyboga@gmail.com>
2024-10-22 14:50:54 +0000

    Add getter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,9 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof(name) !== 'string') {
+    if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    if (typeof(length) !== 'number') {
+    if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     if (!Array.isArray(students)){
