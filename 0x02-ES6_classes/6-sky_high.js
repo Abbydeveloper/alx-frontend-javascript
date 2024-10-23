0b2d8860e37001d3e9a6fa70a3da1978be7877bc
Adeboga Abigail <abbyboga@gmail.com>
2024-10-23 18:10:31 +0000

    sky high

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from '5-building';
+
+export default class SkyHighBuilding {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(val) {
+    if (typeof val === 'number') {
+      this._floors = val;
+    }
+  }
+
+  static evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
