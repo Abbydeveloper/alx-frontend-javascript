fdf865bfbaeee06699f5c13a560fb57aa8828098
Adeboga Abigail <abbyboga@gmail.com>
2024-10-23 12:49:19 +0000

    Add

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -13,7 +13,7 @@ export default class Building {
   }
 
   evacuationWarningMessage() {
-    if (!this.constructor.hasOwnProperty(evacuationWarningMessage)) {
+    if (!this.constructor.hasOwnProperty('evacuationWarningMessage')) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
