b5e7ecd1cdef02987fd2e909f5a45ef2ecec967f
Adeboga Abigail <abbyboga@gmail.com>
2024-10-23 15:00:29 +0000

    Fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,7 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== 'Building'
-        && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    this.evacuationWarningMessage();
   }
 
   get sqft() {
@@ -12,6 +9,14 @@ export default class Building {
   }
 
   set sqft(val) {
+    if (typeof val !== 'number') throw new Error();
     this._sqft = val;
+  }
+
+  // eslint-disable-next-line class-method-use-this
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 }
