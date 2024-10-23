424baa5ab3738dab4b09b52b9522af6c449f624c
Adeboga Abigail <abbyboga@gmail.com>
2024-10-23 12:41:25 +0000

    Add

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(val) {
+    this._sqft = val;
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor.hasOwnProperty(evacuationWarningMessage) {
+      throw new
+    }
+  }
+}
