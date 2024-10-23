f8d965a2e353eacaf82f5825bfa3c2c46cf0cd9a
Adeboga Abigail <abbyboga@gmail.com>
2024-10-23 18:36:44 +0000

    Primitive - Holberron class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `$this._size`;
+  }
+
+  get [Symbol.toNumberTag]() {
+    return `$this._location`;
+  }
+}
