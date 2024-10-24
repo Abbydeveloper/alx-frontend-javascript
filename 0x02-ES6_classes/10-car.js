7e7d43c8a6fd9b3b6fe1cbc12c7571c69e3aef40
Adeboga Abigail <abbyboga@gmail.com>
2024-10-24 11:42:33 +0000

    Return new object of the class

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,7 +5,12 @@ export default class Car {
     this._color = color;
   }
 
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
-    return new (this.constructor[Symbol.species])();
+    const classObj = this.constructor[Symbol.species];
+    return new classObj();
   }
 }
