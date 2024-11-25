897aa5ed541992920e0b3d355861ebf23d792e59
Adeboga Abigail <abbyboga@gmail.com>
2024-11-25 12:19:08 +0000

    Add Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+
